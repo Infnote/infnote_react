e37a74b4111dec0d3c2f0d61c7cce8caf19e586a
haoqianzhang <haoqian.zhang@connect.ust.hk>
2019-02-26 19:51:16 +0800

    Update settings.js

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,6 +1,6 @@
 const SETTINGS = {
-    apiAddress: 'http://hk.infnote.com',
-    chainAddress: 'http://hk.infnote.com',
+    apiAddress: 'https://hk.infnote.com',
+    chainAddress: 'https://hk.infnote.com',
     languagesList: ['English','Simplified Chinese'],
     defaultLanguage: 'en-US'
 }
