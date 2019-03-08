b0c4d03df426de471bee2c07c2d1ed2084ea17eb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-08 14:18:58 +0000

    Deploy io.infnote.com to github.com/Infnote/infnote_react.git:gh-pages

---
+++ precache-manifest.6aad107242f8a51eb79655f50f810574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2390991f52458f7e0f73",
+    "url": "/static/css/main.c749b1cb.chunk.css"
+  },
+  {
+    "revision": "2390991f52458f7e0f73",
+    "url": "/static/js/main.2390991f.chunk.js"
+  },
+  {
+    "revision": "1f1eff8a6d5dce8c8058",
+    "url": "/static/js/1.1f1eff8a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8223000bcaca137dda1f976916817a82",
+    "url": "/static/media/infnote-logo.8223000b.png"
+  },
+  {
+    "revision": "00df53e1c4be6cd6d43b7052cd723ff4",
+    "url": "/index.html"
+  }
+];
